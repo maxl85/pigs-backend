6bd682301cacf0cc8a228a43474d4cd3a016bd2f
maxl85 <maxl85@mail.ru>
2023-09-03 15:48:44 +0300

    findBetweenDates

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get, Post, Response, Param, UseInterceptors, UploadedFile, UseGuards, Body } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags, ApiConsumes, ApiBody } from '@nestjs/swagger';
+import { Controller, Get, Post, Response, Param, UseInterceptors, UploadedFile, UseGuards, Body, Query } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags, ApiConsumes, ApiBody, ApiQuery } from '@nestjs/swagger';
 import { FileInterceptor } from '@nestjs/platform-express';
 
 import { FilesService } from './files.service';
@@ -48,6 +48,32 @@ export class FilesController {
   @ApiBearerAuth()
   findAll(@UserId() userId: number) {
     return this.filesService.findAll(userId);
+  }
+  
+  // http://127.0.0.1:7777/api/files/dates?cam1=cam1_1&cam2=cam1_2'
+  @Get('/dates')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiQuery({ name: 'cam2',  example: 'cam1_2', required: false })
+  @ApiQuery({ name: 'cam1', example: 'cam1_1', required: false })
+  findAllDates(@Query('cam1') cam1: string, @Query('cam2') cam2: string) {
+    return this.filesService.findAllDates(cam1, cam2);
+  }
+  
+  // http://127.0.0.1:7777/api/files/between?cam1=cam1_1&cam2=cam1_2&date1=2023-09-01&date2=2023-09-02
+  @Get('/between')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiQuery({ name: 'cam2',  example: 'cam1_2', required: false })
+  @ApiQuery({ name: 'cam1', example: 'cam1_1', required: false })
+  @ApiQuery({ name: 'date2',  example: '2023-09-02', required: false })
+  @ApiQuery({ name: 'date1', example: '2023-09-01', required: false })
+  findBetweenDates(
+    @Query('cam1') cam1: string,
+    @Query('cam2') cam2: string,
+    @Query('date1') date1: string,
+    @Query('date2') date2: string) {
+      return this.filesService.findBetweenDates(cam1, cam2, date1, date2);
   }
   
   @Get(':path')
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Db, Repository } from 'typeorm';
 
 import { FileEntity } from './entities/file.entity';
 
@@ -16,6 +16,46 @@ export class FilesService {
     qb.where('file.userId = :userId', { userId });
     return qb.getMany();
   }
+  
+  // findAllDates(cam1: string, cam2: string) {
+  //   return this.repository
+  //     .createQueryBuilder('file')
+  //     // .select('DISTINCT file."dateTime"::date', 'dateTime')
+  //     // .select('DISTINCT ("dateTime"::date)')
+  //     // .select('COUNT(DISTINCT("dateTime"::date))')
+  //     .select('DISTINCT("dateTime"::date)')
+  //     // .select('"dateTime"')
+  //     // .select('"dateTime"::date')
+  //     // .distinct(true)
+  //     .where('"camId" = :cam1', { cam1 })
+  //     .orWhere('"camId" = :cam2', { cam2 })
+  //     .orderBy('"dateTime"', 'DESC')
+  //     // .getMany();
+  //     .getRawMany();
+  // }
+  
+  async findAllDates(cam1: string, cam2: string) {
+    const qb = this.repository.createQueryBuilder('files');
+    qb.select('DISTINCT "dateTime"::date');
+    qb.where('"camId" = :cam1', { cam1 });
+    qb.orWhere('"camId" = :cam2', { cam2 });
+    qb.orderBy('"dateTime"', 'DESC');
+    
+    // console.log(qb.getQueryAndParameters())
+    return qb.getRawMany();
+  }
+  
+  async findBetweenDates(cam1: string, cam2: string, date1: string, date2: string) {
+    const qb = this.repository.createQueryBuilder('files');
+    qb.where('"dateTime" >= :date1::date', { date1 });
+    qb.andWhere('"dateTime" < (:date2::date + \'1 day\'::interval)', { date2 });
+    qb.andWhere('("camId" = :cam1 OR "camId" = :cam2)', { cam1, cam2 });
+    qb.orderBy('"dateTime"', 'DESC');
+    
+    // console.log(qb.getQueryAndParameters())
+    return qb.getMany();
+  }
+  
   
   saveFiles(camId: string, dateTime: Date, file: Express.Multer.File, userId: number) {
     // const dateArray = file.filename.split('-').map((item)=>parseInt(item));
